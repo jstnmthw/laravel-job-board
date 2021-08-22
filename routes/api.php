9f700a1869623f115eb956fba0b7b735ebac3092
Justin <justin@fazwaz.com>
2021-08-22 19:19:07 +0700

    added: Geo API routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Resources\UserDataResource;
 use App\Http\Resources\UserResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\auth\LoginController;
@@ -35,3 +34,15 @@ Route::group(['middleware' => ['auth:sanctum', 'throttle:60,1']], function () {
  * Unprotected API
  */
 Route::post('/login', [LoginController::class, 'login']);
+
+/** Geo API */
+Route::group(['prefix' => 'geo'], function() {
+//    Route::get('search/{name}/{parent_id?}', 	'\Igaster\LaravelCities\GeoController@search');
+    Route::get('item/{id}', 		'\Igaster\LaravelCities\GeoController@item');
+    Route::get('children/{id}', 	'\Igaster\LaravelCities\GeoController@children');
+    Route::get('parent/{id}', 	'\Igaster\LaravelCities\GeoController@parent');
+    Route::get('country/{code}',	'\Igaster\LaravelCities\GeoController@country');
+    Route::get('countries', 		'\Igaster\LaravelCities\GeoController@countries');
+    Route::get('ancestors/{id}','\Igaster\LaravelCities\GeoController@ancestors');
+    Route::get('breadcrumbs/{id}','\Igaster\LaravelCities\GeoController@breadcrumbs');
+});
