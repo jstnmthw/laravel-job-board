fecd006a79c63edd2a595d187c006a7239f5c0d2
Justin <justin@fazwaz.com>
2021-08-22 21:50:33 +0700

    rename: UserResource.php to UserController.php

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Ramsey\Uuid\Type\Integer;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(['success' => 'User account updated']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id): JsonResponse
+    {
+        if ($request->user()->id !== $id) {
+            return response()->json(['error' => 'You can only edit your own account'], 403);
+        }
+        User::query()->findOrFail($id)->update($request->all());
+        return response()->json(['success' => 'User account updated']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id): Response
+    {
+        //
+    }
+}
