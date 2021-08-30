d280eee7c47787a58ef1dbf03b9ef6bbb1823437
Justin <justin@fazwaz.com>
2021-08-30 21:04:04 +0700

    removed: Unused classes

--- app/Http/Controllers/api/auth/LoginController.php
+++ app/Http/Controllers/api/auth/LoginController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\api\auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
