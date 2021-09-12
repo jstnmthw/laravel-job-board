73f7bf5c731158ee90876598bfec29fe01738610
Justin <justin@fazwaz.com>
2021-09-12 15:31:48 +0700

    removed: dependencies from UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,24 +23,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        // Randomize Country, Province and City ID's (relational)
-//        $geo = Geo::getCountry('TH');
-//        $provinces = $geo->children()->get();
-//        $province = $provinces[$this->faker->numberBetween(0, count($provinces) - 1)]['id'];
-//        $cities = Geo::query()->find($province)->children()->get();
-//        $city = $cities[$this->faker->numberBetween(0, count($cities) - 1)];
-
-        $geo = Geo::factory()->count(3)->create();
-
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'dob' => $this->faker->dateTimeBetween('-60 years', '-18 years'),
-            'country_id' => $geo[0]->getKey(),
-            'province_id' => $geo[1]->getKey(),
-            'city_id' => $geo[2]->getKey(),
+            'position' => $this->faker->jobTitle(),
+            'country_id' => $this->faker->randomDigit(),
+            'province_id' => $this->faker->randomDigit(),
+            'city_id' => $this->faker->randomDigit(),
             'address' => $this->faker->address(),
             'zipcode' => $this->faker->postcode(),
             'remember_token' => Str::random(10),
