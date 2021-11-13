c72d05994e3f88194d09690aaeb68bd6e0fbf3a9
Justin Mathew <web@jstn.ly>
2021-11-13 20:54:49 +0700

    added: Search store

---
+++ resources/js/store/modules/search.js
@@ -0,0 +1,22 @@
+/**
+ * Search
+ */
+const state = {
+}
+
+const getters = {
+}
+
+const mutations = {
+}
+
+const actions = {
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
