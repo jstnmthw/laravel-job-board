aca7a00870d257ce25d67125d462c8944c3587ac
Justin Mathew <web@jstn.ly>
2021-11-13 20:54:27 +0700

    update: Organize imports by alphabetical order

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,15 +1,15 @@
-import RequestToken from '@/store/modules/request-token'
 import account from "@/store/modules/account";
 import api from '@/store/modules/api'
+import requestToken from '@/store/modules/request-token'
 import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
     strict: true,
     modules: {
-        requestToken: RequestToken,
         account: account,
         api: api,
+        requestToken: requestToken,
     },
 })
 export default store
