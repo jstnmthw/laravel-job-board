db93b8e50b820a0135e5969fc7ca04f29d7aa5b3
Justin Mathew <web@jstn.ly>
2021-11-14 23:53:33 +0700

    updated: Card.vue dark mode scheme

--- resources/js/components/Job/Card.vue
+++ resources/js/components/Job/Card.vue
@@ -12,13 +12,13 @@
                 {{ job.title }}
             </h2>
             <div class="flex justify-between">
-                <p class="text-sm text-gray-400 dark:text-gray-500">
-                    <svg class="w-5 h-5 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                <p class="text-sm text-gray-400 dark:text-gray-400">
+                    <svg class="w-5 h-5 inline-block dark:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                     {{  job.city }}
-                    <svg class="w-5 h-5 inline-block ml-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                    <svg class="w-5 h-5 inline-block ml-3 dark:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                     {{ salary_from_formatted }}
                 </p>
-                <p class="text-sm-alt text-gray-400 dark:text-gray-600">
+                <p class="text-sm-alt text-gray-400 dark:text-gray-500">
                     <span class="inline-block px-1 text-[10px] rounded bg-orange-50 text-orange-500 dark:bg-gray-900 dark:text-orange-700">
                         New
                     </span>
