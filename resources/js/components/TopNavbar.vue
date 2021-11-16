a16eef5140bfe6190aeb6dbfacd7fc37487322cd
Justin Mathew <web@jstn.ly>
2021-11-16 20:24:52 +0700

    fixed: nav dropdown overlap

--- resources/js/components/TopNavbar.vue
+++ resources/js/components/TopNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav :class="{ 'sticky top-0' : sticky }" class="w-full bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
+    <nav :class="{ 'sticky top-0' : sticky }" class="w-full z-20 bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700">
         <div class="max-w-8xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between py-5">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
