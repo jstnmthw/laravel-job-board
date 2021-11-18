fb2cc55e872568fe76d5d4c3e27f795beb8b7717
Justin Mathew <web@jstn.ly>
2021-11-18 23:14:45 +0700

    update: Forms.vue dark mod

--- resources/js/views/styleguide/Forms.vue
+++ resources/js/views/styleguide/Forms.vue
@@ -1,19 +1,19 @@
 <template>
     <styleguide-layout>
         <div class="md:col-span-3 p-3 md:p-6 lg:p-10">
-            <div class="pb-10 border-b border-gray-200 mb-10">
-                <h1 class="inline-block text-3xl font-bold text-gray-900 tracking-tight">
+            <div class="pb-10 border-b border-gray-200 dark:border-gray-700 mb-10">
+                <h1 class="inline-block text-3xl font-bold text-gray-900 dark:text-gray-200 tracking-tight">
                     Forms
                 </h1>
                 <p class="mt-1 text-lg text-gray-500">Base styles for forms</p>
             </div>
-            <p class="mb-8">
-                Give textual form controls like <span class="px-1 py-0 text-orange-700" style="font-family: Consolas, monospace;">inputs</span>
-                and <span class="px-1 py-0 text-orange-700" style="font-family: Consolas, monospace;">textarea</span> an upgrade with custom styles,
+            <p class="mb-8 dark:text-gray-300">
+                Give textual form controls like <span class="text-orange-700" style="font-family: Consolas, monospace;">inputs</span>
+                and <span class="text-orange-700" style="font-family: Consolas, monospace;">textarea</span> an upgrade with custom styles,
                 sizing, focus states, and more.
             </p>
             <div class="font-base text-md text-gray-500 leading-7">
-                <h2 class="text-sh mb-4 font-bold text-gray-900 group flex whitespace-pre-wrap" id="development">
+                <h2 class="text-sh mb-4 font-bold text-gray-900 dark:text-gray-200 group flex whitespace-pre-wrap" id="development">
                     <span>Basic example</span>
                 </h2>
                 <div class="p-6 bg-gray-100 rounded-t-lg">
@@ -25,7 +25,7 @@
                     <textarea class="w-full p-2 rounded-lg transition resize-none placeholder-gray-300 border-gray-200 border-1 focus:border-orange-400 focus:placeholder-gray-400 focus:border-gray-500 focus:ring focus:ring-orange-100"></textarea>
                 </div>
                 <highlightjs
-                    class="p-6 bg-[#2b303b] text-[12px] leading-5 rounded-b-lg mb-10 overflow-x-scroll"
+                    class="p-6 bg-[#2b303b] leading-5 rounded-b-lg mb-10 overflow-hidden"
                     language="js"
                     :code="basicExample"
                 />
@@ -34,12 +34,12 @@
         <div class="md:col-span-1 z-10 sticky h-[calc(100vh-82px)] p-3 md:p-6 lg:p-10 top-[82px] overflow-auto">
             <ul class="overflow-x-hidden text-gray-500 font-medium">
                 <li>
-                    <h5 class="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                    <h5 class="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900 dark:text-gray-200">
                         On this page
                     </h5>
                     <ul>
                         <li>
-                            <a class="block transform transition-colors duration-200 py-2 hover:text-gray-900" href="#">
+                            <a class="block transform transition-colors duration-200 py-2 hover:text-gray-900 dark:hover:text-gray-400" href="#">
                                 <span class="rounded-md absolute inset-0 bg-cyan-50 opacity-0"></span>
                                 <span class="relative">Basic example</span>
                             </a>
@@ -77,4 +77,11 @@ export default {
 
 <style>
 @import '/css/highlightjs/ocean.min.css';
+.hljs {
+    overflow: hidden !important;
+}
+.hljs ::selection {
+    color: white;
+    background: #1c40ff;
+}
 </style>
