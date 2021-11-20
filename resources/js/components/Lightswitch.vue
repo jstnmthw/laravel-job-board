0699182714ee3c0e51afa36c63848bb861d13bcc
Justin Mathew <web@jstn.ly>
2021-11-20 18:59:19 +0700

    update: dark mode switch

---
+++ resources/js/components/Lightswitch.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-if="darkMode" @click="setDarkMode" class="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 mx-1 transition">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+    </button>
+    <button v-else @click="setDarkMode" class="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 mx-1 transition">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Lightswitch",
+    data() {
+        return {
+            darkMode: 0,
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('darkMode') === 'true') {
+            this.setDarkMode();
+        }
+    },
+    computed: {
+    },
+    methods: {
+        setDarkMode() {
+            this.darkMode = !this.darkMode
+            document.getElementsByTagName('html').item(0).classList.toggle('dark');
+            localStorage.setItem('darkMode', this.darkMode)
+        }
+    }
+}
+</script>
