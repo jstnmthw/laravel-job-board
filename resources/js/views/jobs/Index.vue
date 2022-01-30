71f71d838af54c0e09bd07504bcc481b0ae5c1e1
Justin Mathew <web@jstn.ly>
2022-01-30 14:04:19 +0700

    fixed: Match button size

--- resources/js/views/jobs/Index.vue
+++ resources/js/views/jobs/Index.vue
@@ -63,7 +63,7 @@
                             <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             Save
                         </button>
-                        <a href="#" class="inline-block relative font-semibold text-white bg-orange-600 rounded border-b-2 border-orange-700 px-4 py-3 active:top-[2px] active:border-0">
+                        <a href="#" class="inline-block relative font-semibold text-white bg-orange-600 rounded border-b-2 border-orange-700 border px-4 py-2 active:top-[2px] active:border-0">
                             Apply Now
                         </a>
                     </div>
